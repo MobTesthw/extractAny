47ccc0642298b6944828b19a86073dad0a6d354a
Dmitriy <trimula.service@gmail.com>
2023-03-23 17:18:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+import argparse
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Main section
+if __name__ == '__main__':
+
+    for i in range(4):
+        print_hi(f'name: {i} ')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
