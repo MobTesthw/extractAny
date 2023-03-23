ba6a00fade7b5f78df62e7cb24c0d875858013f4
Dmitriy <trimula.service@gmail.com>
2023-03-23 17:34:12 +0100

    added arguments parsing

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ import argparse
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_arg(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Main section
 if __name__ == '__main__':
+    # Arguments processing
+    parser = argparse.ArgumentParser(
+        prog='extractAny',
+        description='Script to unpack nested mixed (zip/gz/tar) archives',
+        epilog='Please enter the parameters'
+    )
+    parser.add_argument('filename')  # positional argument
+    parser.add_argument('-c', '--count')  # option that takes a value
+    parser.add_argument('-v', '--verbose',
+                        action='store_true')  # on/off flag
+    # parser.add_argument('arg1')
 
-    for i in range(4):
-        print_hi(f'name: {i} ')
+    # args, unknown = parser.parse_known_args()
+    args = parser.parse_args()
+    # for i in range(4):
+    #     print_hi(f'name: {i} ')
+    # for arg in range(args.r):
+    #     print_arg(f'name: {arg.numerator} ')
 
+    print_arg(f'File name: {args.filename} ')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
